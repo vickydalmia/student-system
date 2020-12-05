a8b04f02bc30f975cf983131473ac076b7699c61
Vicky Kumar <vickyk.edu@gmail.com>
2020-12-05 15:43:35 +0530

    project done

--- src/utils/fileReader.js
+++ src/utils/fileReader.js
@@ -1,3 +1,4 @@
+import FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 export const readUploadedFileAsText = (inputFile) => {
     const temporaryFileReader = new FileReader();
@@ -12,7 +13,7 @@ export const readUploadedFileAsText = (inputFile) => {
             const wb = XLSX.read(bstr, { type: 'binary' });
             const wsname = wb.SheetNames[0];
             const ws = wb.Sheets[wsname];
-            const data = XLSX.utils.sheet_to_json(ws, { header: 1 , raw: false});
+            const data = XLSX.utils.sheet_to_json(ws, { header: 1, raw: false });
             resolve(data);
         };
         temporaryFileReader.readAsBinaryString(inputFile);
@@ -26,4 +27,14 @@ export const checkfile = (file, validExts) => {
         return false;
     }
     return true;
+}
+const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const fileExtension = '.xlsx';
+
+export const exportToCSV = (csvData, fileName) => {
+    const ws = XLSX.utils.json_to_sheet(csvData);
+    const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
 }
